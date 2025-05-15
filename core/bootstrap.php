260dcb362ebc788c0c1b1eba701dc718dbc1fc50
VovchikMusulman <slavaatam83@gmail.com>
2025-05-15 11:49:37 +0700

    Development of the basic application architecture 2 Middleware Integration

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,6 +4,8 @@ const DIR_CONFIG = '/../config';
 
 //Подключение автозагрузчика composer
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../routes/web.php';
+$app = new Src\Application(new Src\Settings(getConfigs()));
 
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
@@ -21,3 +23,10 @@ function getConfigs(string $path = DIR_CONFIG): array
 require_once __DIR__ . '/../routes/web.php';
 
 return new Src\Application(new Src\Settings(getConfigs()));
+
+function app() {
+    global $app;
+    return $app;
+}
+
+return $app;
